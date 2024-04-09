f90a3affd0b5ae078942992a29ed0f5c0ac5fdcd
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-04-09 13:31:25 +0530

    Create home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Home = () => (
-  <main className="bg-mall flex-1 flex justify-center flex-col items-center">
-    <h1>Shopping Cart</h1>
+  <main className="bg-mall flex-1 bg-cover gap flex flex-col justify-center items-center gap-5">
+    <div className="bg-zinc-800/50 p-3 rounded-sm shadow-sm flex justify-center items-center flex-col gap-5">
+      <h1 className="text-4xl font-bold text-slate-100">The one place where you can get anything!</h1>
+      <p className="text-lg text-zinc-50">This is a mock shopping cart created using tailwind CSS and React</p>
+    </div>
+    <Link to='/' className="bg-violet-700 text-violet-50 p-3 text-xl rounded-md hover:bg-violet-500 active:bg-violet-800">Shop Now!</Link>
   </main>
 );
 
