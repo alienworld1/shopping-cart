f87ab9b7afbcf67d1669881728a621bbb4da294c
alienworld <69631697+alienworld1@users.noreply.github.com>
2024-04-10 07:00:34 +0000

    Add a useFakeItem hook

--- src/apis/fakestore.js
+++ src/apis/fakestore.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 const fetchItems = async number => {
   const response = await fetch(`https://fakestoreapi.com/products?limit=${number}`);
   if (!response.ok) {
@@ -6,4 +8,27 @@ const fetchItems = async number => {
   return await response.json();
 }
 
-export { fetchItems };
+const useFakeItem = id => {
+  useEffect(() => {
+    const [item, setItem] = useState();
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(true);
+  
+    const fetchFakeItem = async () => {
+      try {
+        const fakeItem = await fetchItems(id);
+        setItem(fakeItem);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    
+    fetchFakeItem();
+  }, [])
+
+  return {item, error, loading};
+}
+
+export { fetchItems, useFakeItem };
