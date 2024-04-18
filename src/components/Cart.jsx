5054bf60428a2f70a7f20fd6a8ee434440cc85e1
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-04-18 21:09:15 +0530

    Display total amount in cart page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -23,16 +23,21 @@ const Cart = () => {
   return (
     <main className="flex-1 bg-mall bg-cover flex justify-center items-center">
       <article className="bg-violet-50 p-4 rounded-md shadow-md">
-        <h2 className="text-zinc-950 text-3xl border-b-2 border-violet-800 py-2 mb-5">Your Cart</h2>
+        <h2 className="text-zinc-950 text-3xl font-bold border-b-2 border-violet-800 py-2 mb-5">Your Cart</h2>
         {cartInfo.size() === 0 ?
           <p className="text-xl my-2">Your cart is currently empty at the moment.</p> :
           cartInfo.products().map(productMap => {
             console.log(productMap);
             const product = cartInfo.productFromId(productMap[0]);
             totalAmount += product.price * productMap[1];
-            return <ProductDisplay key={productMap[0]} product={product} quantity={productMap[1]}/>
+            return <ProductDisplay key={productMap[0]} product={product} quantity={productMap[1]}/>;
           })
-        }     
+        }
+        {totalAmount !== 0 && 
+          <div className="text-right mt-3">
+            <h3 className="font-bold text-2xl">Total: ${totalAmount.toFixed(2)}</h3>
+            <button className="bg-violet-800 text-violet-50 rounded-md p-2 mt-4 hover:bg-violet-500 active:bg-violet-900">Proceed to Buy</button>
+          </div>}     
       </article>
     </main>
   );
